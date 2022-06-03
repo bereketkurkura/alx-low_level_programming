22f4d7cc6e8a6f3c2eba87a54e7a946510cc5231
bereketkurkura <bereketkurkura@gmail.com>
2022-06-03 14:29:28 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,18 +14,18 @@
 	{
 
 		for (m = 49; m <= 57; m++)
-			{
+		{
 			if (m > n)
 			{
-	putchar(n);
-	putchar(m);
-	if (n != 56 || m != 57)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	}
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
