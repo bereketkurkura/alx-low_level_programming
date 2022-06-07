a11030f325c17b93108da015988083ef09a7ebe3
bereketkurkura <bereketkurkura@gmail.com>
2022-06-07 15:13:53 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-	
+
 	hours_max = 58;
 	hours_tens = '0';
 	while (hours_tens < '3')
@@ -35,7 +35,6 @@ void jack_bauer(void)
 					_putchar(minutes_ones);
 					_putchar('\n');
 					minutes_ones++;
-		  
 				}
 				minutes_ones = '0';
 				minutes_tens++;
